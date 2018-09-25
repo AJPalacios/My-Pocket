8cdfcd5fbc0abf9d7d337c28be214cbdff293df3
Adan Palacios <ajpalacios.sist@gmail.com>
2018-09-25 00:31:25 -0500

    se arreglo conflicto en app.js

--- app.js
+++ app.js
@@ -76,12 +76,11 @@ app.use('/', index);
 const auth = require('./routes/auth')
 app.use('/', auth)
 
-<<<<<<< HEAD
 const users = require('./routes/users')
 app.use('/users', users)
-=======
+
 const ingresos = require('./routes/ingresos')
 app.use('/', ingresos)
->>>>>>> develop
+
 
 module.exports = app;
