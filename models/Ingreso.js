0b479098c4377b0ffdce4ca18450c99cb88f631b
Mishel <mishel_gonzalez@hotmail.com>
2018-09-24 23:04:43 -0500

    Se creó el modelo  Ingreso

---
+++ models/Ingreso.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const ingresoSchema = new Schema({ 
+  usuario: {
+    type: Schema.Types.ObjectId,
+    ref: "User"
+  },
+  concepto: String,
+  cantidad:String,
+  periodicidad:{
+    type: String,
+    enum: ['Diario', 'Semanal', 'Quincenal', 'Mensual', 'Anual'],
+    default: 'Mensual'
+  }
+},{
+  timestamps:{
+    createdAt: 'created_at',
+    updatedAt: 'updated_at'
+  }
+})
+
+module.exports = mongoose.model('Ingreso', ingresoSchema)
