f8e53d406c31b2a41deec169ec59978c2179f7d9
Mishel <mishel_gonzalez@hotmail.com>
2018-09-25 12:42:41 -0500

    cambio en ingresos

--- routes/ingresos.js
+++ routes/ingresos.js
@@ -2,20 +2,67 @@ const router = require('express').Router()
 const Ingreso = require ('../models/Ingreso')
 const User = require ('../models/User')
 
-router.get('/nuevo_ingreso', (req, res)=>{
-  res.render ('ingresos/nuevo_ingreso')
+
+//NUEVO INGRESO
+router.get('/new', (req, res)=>{
+  res.render ('ingresos/new')
 })
 
-// router.post ('/nuevo_ingreso', (req, res, next)=>{
-//   Ingreso.create(req.body)
-//   .then(ingreso =>{
-//     User.findByIdAndUpdate(req.user._id,{$push:{ingreso:ingreso._id}}, {new:true})
-//     .then(user=>{
-//       console.log(user)
-//       req.app.locals.loggedUser = user
-//       res.redirect('/tablero')
-//     })
-//   })
-// })
+router.post ('/new', (req, res, next)=>{
+  Ingreso.create(req.body)
+  .then(ingreso =>{
+    res.redirect('/ingresos/lista')
+  }).catch(e=>console.log(e))
+})
+
+//LISTA DE INGRESOS
+router.get('/lista', (req, res, next)=>{
+  Ingreso.find()
+  .then (ingresos=>{
+    res.render('ingresos/lista', {ingresos})
+  }) .catch(e=>console.log(e))
+})
+
+
+//DETALLE DE INGRESO
+
+router.get('/detalle/:id', (req, res, next)=>{
+  const {id} = req.params
+  Ingreso.findById(id)
+  .then(ingresos=>{
+    res.render('ingresos/detalle',ingresos)
+  })
+  .catch(e=>console.log(e)) 
+})
+
+
+//EDITAR
+
+router.get('/editar/:id', (req, res, next)=>{
+  const {id} = req.params
+  Book.findById(id)
+    .then(book=>{
+      res.render('book-edit-form', book)
+    })
+    .catch (error=>next(error))
+})
+
+router.post('/edit/:id', (req, res, next)=>{
+  const {id} = req.params
+  Book.findByIdAndUpdate(id, {$set:req.body}, {new: true})
+  .then(book=>{
+    console.log(book)
+    res.redirect(`/books/detail/${id}`)
+  })
+  .catch (error=>next(error))
+})
+
+
+//BORRAR
+router.get('/borrar/:id', (req, res, next)=>{
+  const {id} = req.params
+  Book.deleteOne(id)
+})
+
 
 module.exports = router
