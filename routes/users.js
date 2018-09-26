50c2630f1c10edf354f2b32fcc54d32b8787d775
Mishel <mishel_gonzalez@hotmail.com>
2018-09-26 17:26:44 -0500

    editando sidenav

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,10 @@ const router = require('express').Router()
 const User = require('../models/User')
 const passport = require('../helpers/passport')
 
+const multer = require('multer')
+const uploads = multer({dest:'public/uploads'})
+const uploadCloud =require('../helpers/cloudinary')
+
 const isLogged = (req,res,next)=>{
   if (req.isAuthenticated())return next()
     return res.redirect('/login')
@@ -22,6 +26,24 @@ router.get('/:id',isLogged, (req,res,next)=>{
   })
 })
 
+
+//foto de perffil
+
+// router.get('/users/edit/:id', (req, res, next)=>{
+//   User.findById(req.user._id)
+//     .then(user=>{
+//       res.render(`users/edit/${_id}`,user)
+//     }).catch(e=>next(e))
+// })
+
+// router.post('/users/edit',uploadCloud.single('image'), (req, res, next)=>{
+//   if(req.file)req.body['photoURL'] = req.file.url 
+//   //`uploads/${req.file.filename}`
+//   User.findByIdAndUpdate(req.user._id,{$set:req.body},{new: true})
+//     .then(user=>{
+//       res.redirect('/profile')
+//     }).catch(e=>next(e))
+// })
 
 
 
