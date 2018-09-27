08e77e27ffb614b59dfffeba2fc4fbbf5d22a3d2
Mishel <mishel_gonzalez@hotmail.com>
2018-09-26 21:07:33 -0500

    chart3 - gastos

---
+++ public/javascripts/chart3.js
@@ -0,0 +1,47 @@
+const ctx = document.getElementById('canvas3').getContext('2d')
+
+const drawGraph3 = () => {
+  return fetch('http://localhost:3000/gastos/list-for-chart')
+  .then(result => result.json())
+  .then(gastos => {
+    console.log(gastos[0].cantidad)
+    cantidad = []
+    labels = []
+    for(i = 0; i < gastos.length; i++){
+      cantidad.push(gastos[i].cantidad)
+      labels.push(gastos[i].nombre)
+    }
+    
+    console.log(cantidad)
+    var myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels,
+        datasets: [{
+
+            data: cantidad,
+            backgroundColor: [
+                '#FF6283',
+                '#009DF8',
+                '#FFCC6A',
+                '#3DC0BF',
+                '#C9CBCF',
+                '#FFB880'
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    
+});
+  })
+}
+    
+drawGraph3()
